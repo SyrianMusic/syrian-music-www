14421b354284431d5f5c0f4cd8c1ccc45e4438c9
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2021-01-17 00:31:33 -0500

    remove mobile scaling breakpoint (for now)

--- components/Hero.js
+++ components/Hero.js
@@ -1,35 +1,50 @@
-import cx from 'classnames';
 import PropTypes from 'prop-types';
 import theme from '../styles/theme';
 import Image from './Image';
 
 const Hero = ({ className, content, image, title }) => (
-  <div className={cx('component-Hero-root', className)}>
+  <div className={className}>
     {title && <h1>{title}</h1>}
     <Image className="component-Hero-image" {...image} />
     {content && <p>{content}</p>}
     <style jsx>{`
-      .component-Hero-root {
+      div {
         width: 100%;
       }
 
-      .component-Hero-root h1,
-      .component-Hero-root p {
+      @media (min-width: ${theme.breakpoint.mobileToDesktop}px) {
+        div {
+          width: ${theme.pxToRem(1461)};
+        }
+      }
+
+      h1,
+      p {
+        padding: 0 ${theme.pxToRem(50)};
         text-align: right;
       }
 
-      .component-Hero-root h1 {
+      @media (min-width: ${theme.breakpoint.mobileToDesktop}px) {
+        h1,
+        p {
+          padding: 0;
+        }
+      }
+
+      h1 {
         font-size: ${theme.pxToRem(65)};
         line-height: ${theme.pxToRem(78)};
         letter-spacing: -0.01em;
       }
 
-      .component-Hero-root :global(.component-Hero-image) {
+      div :global(.component-Hero-image) {
         margin-top: ${theme.pxToRem(45)};
         margin-bottom: ${theme.pxToRem(66)};
+        height: auto;
+        width: 100%;
       }
 
-      .component-Hero-root p {
+      p {
         font-size: ${theme.pxToRem(43)};
         line-height: ${theme.pxToRem(52)};
         letter-spacing: 0.01em;
