ac511bd131f294a8701856a4a8eea8a65ac10a59
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2021-03-25 02:09:52 -0400

    adjust connect page

--- pages/connect/index.js
+++ pages/connect/index.js
@@ -9,37 +9,46 @@ const pageConfig = config.nav.connect;
 const ConnectPage = () => (
   <SiteLayout className="gutters" pathname={pageConfig.href}>
     <Title>Connect</Title>
-    <Typography className="page-Connect-title" variant="h3" as="h1" textAlign="center">
-      Connect
-    </Typography>
-    <Typography size="lg" textAlign="left">
-      We want to hear from&nbsp;you!
-    </Typography>
-    <Typography size="lg" textAlign="left">
-      Email us at{' '}
-      <a href="mailto:info@syrianmusic.org" target="_blank" rel="noopener noreferrer">
-        info@syrianmusic.org
-      </a>
-      . To become a donor,{' '}
-      <a href={pageConfig.links.donate.href} target="_blank" rel="noopener noreferrer">
-        fill out our donation&nbsp;form
-      </a>
-      .
-    </Typography>
-    <Typography size="lg" textAlign="left">
-      Follow us on{' '}
-      <a href={pageConfig.links.instagram.href} target="_blank" rel="noopener noreferrer">
-        Instagram
-      </a>{' '}
-      or&nbsp;
-      <a href={pageConfig.links.facebook.href} target="_blank" rel="noopener noreferrer">
-        Facebook
-      </a>
-      .
-    </Typography>
+    <div>
+      <Typography className="page-Connect-title" variant="h3" as="h1" textAlign="center">
+        Connect
+      </Typography>
+      <Typography size="lg" textAlign="left">
+        We want to hear from&nbsp;you!
+      </Typography>
+      <Typography size="lg" textAlign="left">
+        Email us at{' '}
+        <a href="mailto:info@syrianmusic.org" target="_blank" rel="noopener noreferrer">
+          info@syrianmusic.org
+        </a>
+        . To become a donor,{' '}
+        <a href={pageConfig.links.donate.href} target="_blank" rel="noopener noreferrer">
+          fill out our donation&nbsp;form
+        </a>
+        .
+      </Typography>
+      <Typography size="lg" textAlign="left">
+        Follow us on{' '}
+        <a href={pageConfig.links.instagram.href} target="_blank" rel="noopener noreferrer">
+          Instagram
+        </a>{' '}
+        or&nbsp;
+        <a href={pageConfig.links.facebook.href} target="_blank" rel="noopener noreferrer">
+          Facebook
+        </a>
+        .
+      </Typography>
+    </div>
     <style jsx>{`
-      .page-Connect-title {
-        margin-bottom: ${theme.pxToRem(101)};
+      div :global(.page-Connect-title) {
+        margin-top: ${theme.pxToRem(8)};
+        margin-bottom: ${theme.pxToRem(23)};
+      }
+
+      @media screen and (min-width: ${theme.breakpoint.mobileToDesktop}px) {
+        div :global(.page-Connect-title) {
+          margin-bottom: ${theme.pxToRem(101)};
+        }
       }
     `}</style>
   </SiteLayout>
