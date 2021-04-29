4ad483ddb9fed96385f53cc6fe71647a9e716ff7
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2021-04-28 23:40:53 -0400

    add karin nangreave

--- data/people.js
+++ data/people.js
@@ -1,87 +1,107 @@
 import { apostrophe } from '../utils/unicode';
 
+const KEYS = {
+  fouadSalloum: 'fouad-salloum',
+  hannaMadbak: 'hanna-madbak',
+  homamIbrahim: 'homam-ibrahim',
+  karinNangreave: 'karin-nangreave',
+  marissaArciola: 'marissa-arciola',
+  samerAli: 'samer-ali',
+};
+
+class Child {
+  constructor({ key, text } = {}) {
+    this.key = key;
+    this.text = text;
+  }
+
+  format(index) {
+    const key = `${this.key}-${index + 1}`;
+
+    return {
+      _type: 'span',
+      _key: key,
+      text: this.text,
+      marks: [],
+    };
+  }
+}
+
+class Block {
+  constructor({ children = [], key } = {}) {
+    if (!Array.isArray(children)) {
+      throw new Error('Block children are not an array.');
+    }
+    this.children = children.map((childConfig) => new Child({ ...childConfig, key: this.key }));
+    this.key = key;
+  }
+
+  // TODO: Actually implement this
+  format(index) {
+    const key = `${this.key}-${index + 1}`;
+
+    return {
+      _type: 'block',
+      _key: key,
+      style: 'normal',
+      children: this.children.map((child, childIndex) => child.format({ ...childIndex, key })),
+      markDefs: [],
+    };
+  }
+}
+
 export default {
-  'samer-ali': {
+  [KEYS.samerAli]: {
     name: 'Samer Ali',
     title: 'Founder and Artistic Director',
     bio: [
-      {
-        _type: 'block',
-        _key: 'samer-ali-1',
-        style: 'normal',
+      new Block({
+        key: KEYS.samerAli,
         children: [
           {
-            _type: 'span',
-            _key: 'samer-ali-1-1',
             text:
               'A native of Syria, Samer Ali is a physician, violinist, composer, founder and artistic director of the Syrian Music Preservation Initiative.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
-      {
-        _type: 'block',
-        _key: 'samer-ali-2',
-        style: 'normal',
+      }),
+      new Block({
+        key: KEYS.samerAli,
         children: [
           {
-            _type: 'span',
-            _key: 'samer-ali-2-1',
             text:
               'Samer has led Takht al-Nagham, SMPI' +
               apostrophe +
               's ensemble, in New York at Alwan for the Arts, Scandinavia House, and Roulette Intermedium; as well as at the Kennedy center in Washington, DC with Syrian soprano Lubana al-Quntar.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
-      {
-        _type: 'block',
-        _key: 'samer-ali-3',
-        style: 'normal',
+      }),
+      new Block({
+        key: KEYS.samerAli,
         children: [
           {
-            _type: 'span',
-            _key: 'samer-ali-3-1',
             text:
               'He began studying western classical violin at the age of eight with Fawaz al-Ali and Ali Farran, and later pursued intensive conservatory studies with Ali Mukhtar Babayev. He studied the Arab music traditions with Simon Shaheen, Anwar Hariri, and Muhammad Qadri Dalal.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
-      {
-        _type: 'block',
-        _key: 'samer-ali-4',
-        style: 'normal',
+      }),
+      new Block({
+        key: KEYS.samerAli,
         children: [
           {
-            _type: 'span',
-            _key: 'samer-ali-4-1',
             text:
               'In Damascus, he co-founded Awj Ensemble, and has continued to perform in the US with groups like the Bronx Orchestra and the National Arab Orchestra.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
-      {
-        _type: 'block',
-        _key: 'samer-ali-5',
-        style: 'normal',
+      }),
+      new Block({
+        key: KEYS.samerAli,
         children: [
           {
-            _type: 'span',
-            _key: 'samer-ali-5-1',
             text:
               'In medicine, Samer received his M.D. from Syria and later graduated from medical residency in Anatomic and Clinical Pathology at Mount Sinai Hospital. He is currently a Surgical Pathology Fellow at Lenox Hill Hospital in New York.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
+      }),
     ],
     image: {
       src: '/images/people/samer-ali.png',
@@ -99,70 +119,46 @@ export default {
       height: 556,
     },
   },
-  'marissa-arciola': {
+  [KEYS.marissaArciola]: {
     name: 'Marissa Arciola',
     title: 'President',
     bio: [
-      {
-        _type: 'block',
-        _key: 'marissa-arciola-1',
-        style: 'normal',
+      new Block({
+        key: KEYS.marissaArciola,
         children: [
           {
-            _type: 'span',
-            _key: 'marissa-arciola-1-1',
             text:
               'Marissa is a bassist, strategist and leader with a unique combination of artistic and business experience helping her build on founder Samer Ali’s vision of the SMPI.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
-      {
-        _type: 'block',
-        _key: 'marissa-arciola-2',
-        style: 'normal',
+      }),
+      new Block({
+        key: KEYS.marissaArciola,
         children: [
           {
-            _type: 'span',
-            _key: 'marissa-arciola-2-1',
             text:
               'Marissa completed her undergraduate degree in double bass performance at the Eastman School of Music under the tutelage of renowned soloist, James VanDemark and Curtis Buris.  Marissa continued with music while completing her MBA and MA in Arts Administration at SMU, Marissa.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
-      {
-        _type: 'block',
-        _key: 'marissa-arciola-3',
-        style: 'normal',
+      }),
+      new Block({
+        key: KEYS.marissaArciola,
         children: [
           {
-            _type: 'span',
-            _key: 'marissa-arciola-3-1',
             text:
               'After her studies, Marissa has worked with a number of nonprofit organizations while playing with groups ranging from classical, rock, and Middle Eastern music.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
-      {
-        _type: 'block',
-        _key: 'marissa-arciola-4',
-        style: 'normal',
+      }),
+      new Block({
+        key: KEYS.marissaArciola,
         children: [
           {
-            _type: 'span',
-            _key: 'marissa-arciola-4-1',
             text:
               'As President of the SMPI board, and member of Takht al-Nagham, Marissa helps to move the organization forward both programmatically and financially, by putting a focus on the digital initiatives, prioritizing projects and growing donation and fundraising prospects.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
+      }),
     ],
     image: {
       src: '/images/people/marissa-arciola.png',
@@ -180,80 +176,54 @@ export default {
       height: 494,
     },
   },
-  'fouad-salloum': {
+  [KEYS.fouadSalloum]: {
     name: 'Fouad Salloum',
     title: 'Vice President of the Board',
     bio: [
-      {
-        _type: 'block',
-        _key: 'fouad-salloum-1',
-        style: 'normal',
+      new Block({
+        key: KEYS.fouadSalloum,
         children: [
           {
-            _type: 'span',
-            _key: 'fouad-salloum-1-1',
             text:
               'Fouad Salloum is a New York based business manager and amateur musician playing various instruments including oud, qanun, buzuq and ney. In addition to music, Fouad' +
               apostrophe +
               's passions include painting and photography and he has worked with many musical groups in various capacities and many locations.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
-
-      {
-        _type: 'block',
-        _key: 'fouad-salloum-2',
-        style: 'normal',
+      }),
+      new Block({
+        key: KEYS.fouadSalloum,
         children: [
           {
-            _type: 'span',
-            _key: 'fouad-salloum-2-1',
             text:
               'Also an instrument collector, Fouad has amassed a collection of ouds (with a focus on the Syrian luthiers) which can only be described as breathtaking.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
-
-      {
-        _type: 'block',
-        _key: 'fouad-salloum-3',
-        style: 'normal',
+      }),
+      new Block({
+        key: KEYS.fouadSalloum,
         children: [
           {
-            _type: 'span',
-            _key: 'fouad-salloum-3-1',
             text:
-              'Fouad shares his collection, appreciation of art and his expertise with SMPI as the Vice President of the board. ',
-            marks: [],
+              'Fouad shares his collection, appreciation of art and his expertise with SMPI as the Vice President of the board.',
           },
         ],
-        markDefs: [],
-      },
+      }),
     ],
   },
-  'hanna-madbak': {
+  [KEYS.hannaMadbak]: {
     name: 'Hanna Madbak',
     title: 'Legal Counsel',
     bio: [
-      {
-        _type: 'block',
-        _key: 'hanna-madbak-1',
-        style: 'normal',
+      new Block({
+        key: KEYS.hannaMadbak,
         children: [
           {
-            _type: 'span',
-            _key: 'hanna-madbak-1-1',
             text:
               "Hanna Madbak grew up in Beirut, Lebanon. From an early age, he was immersed in the music of the region and developed a deep appreciation of it. Hanna is an avid listener and student of Arabic music and studied oud with the late Director of the Lebanese Conservatory, Bassam Saba. Hanna is a practicing attorney in New York and serves as SMPI's legal advisor as well as being a member of the Board.",
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
+      }),
     ],
     image: {
       src: '/images/people/hanna-madbak.png',
@@ -271,25 +241,19 @@ export default {
       height: 534,
     },
   },
-  'homam-ibrahim': {
+  [KEYS.homamIbrahim]: {
     name: 'Homam Ibrahim',
     title: 'Board Member',
     bio: [
-      {
-        _type: 'block',
-        _key: 'homam-ibrahim-1',
-        style: 'normal',
+      new Block({
+        key: KEYS.homamIbrahim,
         children: [
           {
-            _type: 'span',
-            _key: 'homam-ibrahim-1',
             text:
               'Homam Ibrahim was born in Latakia, Syria and began studying violin at the age of 10 with Bassam Yusuf. While playing with the Music Club of Latakia for many years, he developed a passion and an ear for music. In 2008, Homam came to the United States to pursue his medical training and he currently practices interventional cardiology at NYU. Homam feels strongly that preserving the history and tradition of Syrian music is important and sits on the board of SMPI to help move the many performance, research and education initiatives forward with planning and support.',
-            marks: [],
           },
         ],
-        markDefs: [],
-      },
+      }),
     ],
     image: {
       src: '/images/people/homam-ibrahim.png',
@@ -306,5 +270,20 @@ export default {
       width: 370,
       height: 489,
     },
+  },
+  [KEYS.karinNangreave]: {
+    name: 'Karin Nangreave',
+    title: 'Treasurer',
+    bio: [
+      new Block({
+        key: KEYS.karinNangreave,
+        children: [
+          {
+            text:
+              "Karin Nangreave is a double bassist turned financial director. After attending the Eastman School of Music, she transferred to Northwestern University to pursue a degree in Music Performance. After graduating, Karin then received a Master's of Science in Accounting and received her CPA designation. She became an auditor of nonprofit organizations where she helped advise them on their financial practices and how to properly set up their financial controls for success and compliance. Currently, she works as the Controller at a private school in Chicago for 2 years and has been on other arts nonprofit boards in the past.",
+          },
+        ],
+      }),
+    ],
   },
 };
