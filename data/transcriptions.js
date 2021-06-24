2dd851c5fc7cb230c19103437eb4dcd3e2df7742
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2021-06-24 00:37:49 -0400

    Add new transcriptions (#63)
    * update farran samai bayati
    
    * add new transcriptions

--- data/transcriptions.js
+++ data/transcriptions.js
@@ -1,4 +1,16 @@
 const composers = {
+  ABUALNASR_KHALED: {
+    first: 'Khaled',
+    last: 'Abu al-Nasr',
+  },
+  AJJAN_MAHMOUD: {
+    first: 'Mahmoud',
+    last: 'Ajjan',
+  },
+  ALSHAWWA_SAMI: {
+    first: 'Sami',
+    last: 'al-Shawwa',
+  },
   DARWISH_ALI: {
     first: 'Ali',
     last: 'Darwish',
@@ -11,16 +23,24 @@ const composers = {
     first: 'Jamil',
     last: "'Oweyss",
   },
+  SHALFOUN_ISKANDER: {
+    first: 'Iskander',
+    last: 'Shalfoun',
+  },
 };
 
 const forms = {
+  BASHRAF: 'Bashraf',
   SAMAI: "Sama'i",
 };
 
 const maqams = {
   BAYATI: 'Bayati',
-  NAHAWANT: 'Nahawand',
+  HIJAZ: 'Hijaz',
+  HIJAZ_KAR: 'Hijaz Kar',
+  NAHAWAND: 'Nahawand',
   NAWA_ATHAR: 'Nawa Athar',
+  RAST: 'Rast',
 };
 
 const transcriptions = [
@@ -39,8 +59,32 @@ const transcriptions = [
   {
     composer: composers.DARWISH_ALI,
     form: forms.SAMAI,
-    maqam: maqams.NAHAWANT,
+    maqam: maqams.NAHAWAND,
     filename: 'DarwishAli_SamaiNahawand.pdf',
+  },
+  {
+    composer: composers.AJJAN_MAHMOUD,
+    form: forms.SAMAI,
+    maqam: maqams.HIJAZ,
+    filename: 'AjjanMahmoud_SamaiHijaz.pdf',
+  },
+  {
+    composer: composers.ALSHAWWA_SAMI,
+    form: forms.BASHRAF,
+    maqam: maqams.RAST,
+    filename: 'al-ShawwaSami_BashrafRast.pdf',
+  },
+  {
+    composer: composers.SHALFOUN_ISKANDER,
+    form: forms.SAMAI,
+    maqam: maqams.HIJAZ_KAR,
+    filename: 'ShalfounIskander_SamaiHijazKar.pdf',
+  },
+  {
+    composer: composers.ABUALNASR_KHALED,
+    form: forms.SAMAI,
+    maqam: maqams.NAHAWAND,
+    filename: 'AbuAl-NasrKhaled_SamaiNahawand.pdf',
   },
 ];
 
