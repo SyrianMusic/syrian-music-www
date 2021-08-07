fe37f6de9fd33892441f6896738df9bfbe80f0be
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2021-08-07 12:18:32 -0400

    create standalone signup page

---
+++ pages/sign-up/index.js
@@ -0,0 +1,10 @@
+import SiteLayout from '../../components/SiteLayout';
+import Title from '../../components/Title';
+
+const SignUpPage = () => (
+  <SiteLayout>
+    <Title>Newsletter Sign-Up</Title>
+  </SiteLayout>
+);
+
+export default SignUpPage;
