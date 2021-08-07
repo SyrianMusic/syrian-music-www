ec73a6166bf381afe887054db1d8985d8d5bf94a
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2021-08-07 11:14:19 -0400

    Add transcriptions (#70)
    * add new transcriptions
    
    * add sorting by composer first name

--- data/transcriptions.js
+++ data/transcriptions.js
@@ -1,5 +1,5 @@
 const composers = {
-  ABUALNASR_KHALED: {
+  ABU_AL_NASR_KHALED: {
     first: 'Khaled',
     last: 'Abu al-Nasr',
   },
@@ -7,7 +7,11 @@ const composers = {
     first: 'Mahmoud',
     last: 'Ajjan',
   },
-  ALSHAWWA_SAMI: {
+  AJJAN_ZIAD: {
+    first: 'Ziad',
+    last: 'Ajjan',
+  },
+  AL_SHAWWA_SAMI: {
     first: 'Sami',
     last: 'al-Shawwa',
   },
@@ -18,6 +22,18 @@ const composers = {
   FARRAN_ALI: {
     first: 'Ali',
     last: 'Farran',
+  },
+  HAJ_HUSSEIN_KHALIL: {
+    first: 'Khalil',
+    last: 'Haj Hussein',
+  },
+  JABAQJI_ABDULRAHMAN: {
+    first: 'Abdulrahman',
+    last: 'Jabaqji',
+  },
+  KHASKIYYA_RAMEZ: {
+    first: 'Ramez',
+    last: 'Khaskiyya',
   },
   OWEYSS_JAMIL: {
     first: 'Jamil',
@@ -31,16 +47,22 @@ const composers = {
 
 const forms = {
   BASHRAF: 'Bashraf',
+  LONGA: 'Longa',
+  MUWASHAH: 'Muwashah',
   SAMAI: "Sama'i",
 };
 
 const maqams = {
   BAYATI: 'Bayati',
+  BUSELIK: 'Buselik',
   HIJAZ: 'Hijaz',
   HIJAZ_KAR: 'Hijaz Kar',
+  HUZAM: 'Huzam',
   NAHAWAND: 'Nahawand',
   NAWA_ATHAR: 'Nawa Athar',
+  RAHAT_AL_ARWAH: 'Rahat al-Arwah',
   RAST: 'Rast',
+  SABA_ZAMZAMA: 'Saba Zamzama',
 };
 
 const transcriptions = [
@@ -69,7 +91,7 @@ const transcriptions = [
     filename: 'AjjanMahmoud_SamaiHijaz.pdf',
   },
   {
-    composer: composers.ALSHAWWA_SAMI,
+    composer: composers.AL_SHAWWA_SAMI,
     form: forms.BASHRAF,
     maqam: maqams.RAST,
     filename: 'al-ShawwaSami_BashrafRast.pdf',
@@ -81,10 +103,52 @@ const transcriptions = [
     filename: 'ShalfounIskander_SamaiHijazKar.pdf',
   },
   {
-    composer: composers.ABUALNASR_KHALED,
+    composer: composers.ABU_AL_NASR_KHALED,
     form: forms.SAMAI,
     maqam: maqams.NAHAWAND,
     filename: 'AbuAl-NasrKhaled_SamaiNahawand.pdf',
+  },
+  {
+    composer: composers.AJJAN_MAHMOUD,
+    form: forms.SAMAI,
+    maqam: maqams.SABA_ZAMZAMA,
+    filename: 'AjjanMahmoud_SamaiSaba Zamzama.pdf',
+  },
+  {
+    composer: composers.AJJAN_ZIAD,
+    form: forms.LONGA,
+    maqam: maqams.BUSELIK,
+    filename: 'AjjanZiad_LongaBuselik.pdf',
+  },
+  {
+    composer: composers.AJJAN_ZIAD,
+    form: forms.LONGA,
+    maqam: maqams.HUZAM,
+    filename: 'AjjanZiad_LongaHuzam.pdf',
+  },
+  {
+    composer: composers.AJJAN_MAHMOUD,
+    form: forms.SAMAI,
+    maqam: maqams.RAHAT_AL_ARWAH,
+    filename: 'AjjanMahmoud_SamaiRahatal-Arwah.pdf',
+  },
+  {
+    composer: composers.HAJ_HUSSEIN_KHALIL,
+    form: forms.MUWASHAH,
+    maqam: maqams.RAHAT_AL_ARWAH,
+    filename: 'HajHusseinKhalil_MuwashahRahatal-Arwah.pdf',
+  },
+  {
+    composer: composers.JABAQJI_ABDULRAHMAN,
+    form: forms.SAMAI,
+    maqam: maqams.RAST,
+    filename: 'JabaqjiAbdulrahman_SamaiRast.pdf',
+  },
+  {
+    composer: composers.KHASKIYYA_RAMEZ,
+    form: forms.SAMAI,
+    maqam: maqams.BAYATI,
+    filename: 'KhaskiyyaRamez_SamaiBayati.pdf',
   },
 ];
 
