b7398e2adedd4e8f9ccf40c5b79cbcf9ace6c450
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2021-10-07 17:00:36 -0400

    edit mobile viewport width

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+console.log({ MINIMAL_VIEWPORTS });
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -8,4 +12,17 @@ export const parameters = {
     },
   },
   layout: 'fullscreen',
+  viewport: {
+    viewports: {
+      ...MINIMAL_VIEWPORTS,
+      mobile1: {
+        name: 'Small mobile',
+        styles: {
+          height: '812px',
+          width: '375px',
+        },
+        type: 'mobile',
+      },
+    },
+  },
 };
