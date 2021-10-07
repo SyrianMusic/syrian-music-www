b0c1254197e60d6dbb90300c25d950f4acc00b0f
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2021-10-06 22:29:48 -0400

    add card elements from stripe

--- pages/donate/DonatePage.js
+++ pages/donate/DonatePage.js
@@ -1,3 +1,41 @@
+import { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { CardElement, Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import SiteLayout from '../../components/SiteLayout';
 
-export const DonatePage = () => <SiteLayout className="gutters" pathname="/donate"></SiteLayout>;
+// https://github.com/stripe/react-stripe-js/blob/master/src/index.ts
+// https://stripe.com/docs/js/elements_object/create_element?type=card
+
+export const DonatePage = ({ onSubmit, stripeKey }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(e);
+  };
+
+  const stripe = useMemo(() => loadStripe(stripeKey), [stripeKey]);
+
+  return (
+    <SiteLayout className="gutters" pathname="/donate">
+      <Elements stripe={stripe}>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="card-details">
+            Card details
+            <CardElement id="card-details" options={{}} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </Elements>
+    </SiteLayout>
+  );
+};
+
+DonatePage.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  stripeKey: PropTypes.string.isRequired,
+};
+
+DonatePage.defaultProps = {
+  /** The test key used in the Stripe docs */
+  stripeKey: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+};
--- pages/donate/index.page.js
+++ pages/donate/index.page.js
@@ -1,3 +1,19 @@
+import PropTypes from 'prop-types';
 import { DonatePage } from './DonatePage';
 
-export default DonatePage;
+const DonatePageContainer = ({ stripeKey }) => {
+  const onSubmit = () => {};
+
+  return <DonatePage onSubmit={onSubmit} stripeKey={stripeKey} />;
+};
+
+DonatePageContainer.propTypes = {
+  stripeKey: PropTypes.string.isRequired,
+};
+
+export const getStaticProps = async () => {
+  const stripeKey = process.env.STRIPE_KEY;
+  return { props: { stripeKey } };
+};
+
+export default DonatePageContainer;
