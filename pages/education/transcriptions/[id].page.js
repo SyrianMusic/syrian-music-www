753330102eff0373b2597c089565d25c058a4381
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2021-10-26 18:19:26 -0400

    add title to page

--- pages/education/transcriptions/[id].page.js
+++ pages/education/transcriptions/[id].page.js
@@ -1,9 +1,22 @@
 import PropTypes from 'prop-types';
+import SiteLayout from '../../../components/SiteLayout';
+import Title from '../../../components/Title';
+import Typography from '../../../components/Typography';
 import { MusicalWorkAPI } from '../../../musicalWorks';
 import { musicalWorkPropShape } from './propTypes';
 
 const TranscriptionPage = ({ musicalWork }) => {
-  return <div>{JSON.stringify({ musicalWork })}</div>;
+  return (
+    <SiteLayout
+      className="page-Transcription-SiteLayout"
+      pathname={`/education/transcriptions/${musicalWork?.id}`}>
+      <Title>{musicalWork?.title}</Title>
+      <Typography className="page-Transcription-title" variant="h3" as="h1" textAlign="center">
+        {musicalWork?.title}
+      </Typography>
+      <style jsx>{``}</style>
+    </SiteLayout>
+  );
 };
 
 TranscriptionPage.propTypes = {
