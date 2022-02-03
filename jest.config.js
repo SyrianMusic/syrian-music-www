4cf675a70b7340b84fe9d1c97ee7914ff48129c5
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-02-02 23:14:20 -0500

    support yaml in jest (#114)

--- jest.config.js
+++ jest.config.js
@@ -19,6 +19,7 @@ module.exports = {
     /* Use babel-jest to transpile tests with the next/babel preset
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '\\.yaml$': 'jest-transform-yaml',
   },
   transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
 };
