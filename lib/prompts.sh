e100c675ef70cbba4a5440acaaffefb32ed3b708
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-02-06 01:43:00 -0500

    cleanup

--- lib/prompts.sh
+++ lib/prompts.sh
@@ -1,3 +1,3 @@
 function confirm() {
-    read -p "$1 (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || return 1
+    read -p "$1 (Y/N): " CONFIRM && [[ $CONFIRM == [yY] || $CONFIRM == [yY][eE][sS] ]] || return 1
 }
