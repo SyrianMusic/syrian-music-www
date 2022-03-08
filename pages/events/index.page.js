854be15222f393188afed4596140ea619f8a5e1b
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-03-08 00:36:09 -0500

    redirect /events to /performance

--- pages/events/index.page.js
+++ pages/events/index.page.js
@@ -1,23 +1,12 @@
-import { gql } from '@apollo/client';
-import { BaseAPI } from '../../api';
-import EventsPage from './EventsPage';
+const EventsPage = () => null;
 
 export const getStaticProps = async () => {
-  const query = gql`
-    query events($id: String!) {
-      events(id: $id) {
-        sys {
-          id
-        }
-        content {
-          json
-        }
-      }
-    }
-  `;
-  const variables = { id: '4iJK0Na5QAcDrisQbaFXSL' };
-  const { data } = await BaseAPI.query(query, { variables });
-  return { props: { content: data.events.content.json } };
+  return {
+    redirect: {
+      destination: '/performance',
+      permanent: true,
+    },
+  };
 };
 
 export default EventsPage;
