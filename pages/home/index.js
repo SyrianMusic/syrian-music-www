b1b26017f236414085ee5f055d9e3e7ec8ff124f
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-06-14 20:42:03 -0400

    convert hero to emotion

---
+++ pages/home/index.js
@@ -0,0 +1,56 @@
+import Hero from 'components/Hero';
+import SiteLayout from 'components/SiteLayout';
+import Title from 'components/Title';
+import Typography from 'components/Typography';
+import theme from 'styles/theme';
+import HomeLinks from './HomeLinks';
+
+const HomePage = () => (
+  <SiteLayout pathname="/">
+    <Title>Home</Title>
+    <h1 className="visually-hidden">Home</h1>
+    <Hero
+      css={{
+        marginBottom: theme.pxToRem(48),
+        [theme.mq.mobileToDesktop]: {
+          marginBottom: theme.pxToRem(88),
+        },
+      }}
+      image={{
+        src: '/images/instruments/syrian-oud-black-background.png',
+        width: 1457,
+        height: 820,
+        srcSet: [
+          {
+            densityFactor: 2,
+            src: '/images/instruments/syrian-oud-black-background@2x.png',
+          },
+          {
+            densityFactor: 3,
+            src: '/images/instruments/syrian-oud-black-background@3x.png',
+          },
+        ],
+      }}>
+      <Typography className="page-Home-hero-description" size="md">
+        Welcome to the Syrian Music Preservation Initiative where we promote and celebrate the
+        diverse ethnic and regional musical traditions of&nbsp;Syria.
+      </Typography>
+    </Hero>
+    <HomeLinks className="page-Home-links" />
+    <style global jsx>
+      {`
+        .page-Home-links {
+          margin-bottom: ${theme.pxToRem(39)};
+        }
+
+        @media screen and (min-width: ${theme.breakpoint.mobileToDesktop}px) {
+          .page-Home-links {
+            margin-bottom: ${theme.pxToRem(75)};
+          }
+        }
+      `}
+    </style>
+  </SiteLayout>
+);
+
+export default HomePage;
