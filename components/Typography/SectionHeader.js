1cb07311808e15ba1c95ae864d656361dcd0a418
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-07-04 12:35:14 -0400

    cleanup styles

--- components/Typography/SectionHeader.js
+++ components/Typography/SectionHeader.js
@@ -2,47 +2,24 @@ import PropTypes from 'prop-types';
 import { Typography } from './Typography';
 import theme from '../../styles/theme';
 
-const getVariantStyles = (variant) => {
-  switch (variant) {
-    case Typography.variants.body:
-      return {
-        ':after': {
-          marginBottom: '0.45em',
-        },
-      };
-    case Typography.variants.h3:
-    default:
-      return {
-        ':after': {
-          marginBottom: '0.45em',
-          // [theme.mq.mobileToDesktop]: {
-          //   marginBottom: '0.5em',
-          // },
-        },
-      };
-  }
-};
-
 const SectionHeader = ({ className, children, as, size, variant }) => {
   return (
     <Typography
-      css={[
-        getVariantStyles(variant),
-        {
-          clear: 'both',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          '&:after': {
-            backgroundColor: theme.color.accentTan,
-            content: '""',
-            flex: 1,
-            height: '1px',
-            marginLeft: theme.spacing.get(16),
-            alignSelf: 'flex-end',
-          },
+      css={{
+        clear: 'both',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        '&:after': {
+          backgroundColor: theme.color.accentTan,
+          content: '""',
+          flex: 1,
+          height: '1px',
+          marginLeft: theme.spacing.get(16),
+          marginBottom: '0.45em',
+          alignSelf: 'flex-end',
         },
-      ]}
+      }}
       className={className}
       size={size}
       variant={variant}
