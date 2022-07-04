38a06e676714ce14426b41f50ecea4f2d3fc6ac2
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-07-04 12:15:10 -0400

    convert video to emotion

--- components/Video.js
+++ components/Video.js
@@ -1,30 +1,28 @@
 import PropTypes from 'prop-types';
 
 const Video = ({ className, id, title }) => (
-  <div className={className}>
+  <div
+    className={className}
+    css={{
+      position: 'relative',
+      overflow: 'hidden',
+      paddingTop: '56.25%',
+    }}>
     <iframe
+      css={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        border: 0,
+      }}
       title={title}
       src={`https://www.youtube.com/embed/${id}?color=white&playsinline=1`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
     />
-    <style jsx>{`
-      div {
-        position: relative;
-        overflow: hidden;
-        padding-top: 56.25%;
-      }
-
-      iframe {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        border: 0;
-      }
-    `}</style>
   </div>
 );
 
