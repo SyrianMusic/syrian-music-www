be800b15af2f7f87519dfb70a1a172d1c7013acf
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-08-22 22:48:24 -0400

    check for auth header

--- netlify/functions/__tests__/hello.test.js
+++ netlify/functions/__tests__/hello.test.js
@@ -1,14 +1,24 @@
 import { handler as hello } from '../hello';
 
+const setupSuccessResponse = async () => {
+  const token = 'token';
+  return await hello({ headers: { Authorization: `Bearer ${token}` } });
+};
+
 describe('hello', () => {
-  it('returns a 200 status code', async () => {
-    const res = await hello();
+  it('when passed a valid JWT token, then it returns a 200 status code', async () => {
+    const res = await setupSuccessResponse();
     expect(res.statusCode).toBe(200);
   });
 
-  it('returns a message', async () => {
-    const res = await hello();
+  it('when passed a valid JWT token, then it returns a message', async () => {
+    const res = await setupSuccessResponse();
     const { message } = JSON.parse(res.body);
     expect(message).toBe('Hello World');
+  });
+
+  it('when not passed a JWT token, then it returns a 401 status code', async () => {
+    const res = await hello();
+    expect(res.statusCode).toBe(401);
   });
 });
--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,4 +1,11 @@
-exports.handler = async function () {
+exports.handler = async function (event = {}) {
+  const { headers = {} } = event;
+
+  const hasAuthHeader = 'Authorization' in headers;
+  if (!hasAuthHeader) {
+    return { statusCode: 401 };
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello World' }),
