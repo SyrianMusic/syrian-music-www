130e0f5d0ebd723834a2e6584c7be7a2bfe16229
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-08-22 23:11:24 -0400

    verify JWT token

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,9 +1,22 @@
+import jwt from 'jsonwebtoken';
+import environment from '../../utils/environment';
+
+const unauthorizedResponse = { statusCode: 401 };
+
 exports.handler = async function (event = {}) {
   const { headers = {} } = event;
 
   const hasAuthHeader = 'Authorization' in headers;
   if (!hasAuthHeader) {
-    return { statusCode: 401 };
+    return unauthorizedResponse;
+  }
+
+  const token = headers.Authorization.replace('Bearer ', '');
+
+  try {
+    jwt.verify(token, environment.clientSecret);
+  } catch (e) {
+    return unauthorizedResponse;
   }
 
   return {
