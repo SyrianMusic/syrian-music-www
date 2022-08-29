6fc8b088434c52543a5285c946e842eae3950bf4
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-08-29 01:20:53 -0400

    sort past events with end dates

--- pages/performance/utils.js
+++ pages/performance/utils.js
@@ -1,31 +1,55 @@
-const getSortDate = (start, end) => {
-  const startDate = new Date(start);
-  const endDate = new Date(end);
+const ORDER_ASCENDING = 'asc';
+const ORDER_DESCENDING = 'desc';
 
-  if (startDate < new Date(Date.now()) && endDate) {
-    return endDate;
+const sortDates = (date1, date2, { order = ORDER_ASCENDING } = {}) => {
+  if (date1 > date2) {
+    return order === ORDER_ASCENDING ? 1 : -1;
   }
 
-  return startDate;
+  if (date1 < date2) {
+    return order === ORDER_ASCENDING ? -1 : 1;
+  }
+
+  return 0;
+};
+
+export const sortPastEvents = (events) => {
+  let pastEvents = [];
+
+  if (Array.isArray(events) && events.length) {
+    const getSortDate = (startDate, endDate) => {
+      return endDate ? new Date(endDate) : new Date(startDate);
+    };
+
+    pastEvents = [...events].sort((event1, event2) => {
+      const sortDate1 = getSortDate(event1?.startDate, event1?.endDate);
+      const sortDate2 = getSortDate(event2?.startDate, event2?.endDate);
+      return sortDates(sortDate1, sortDate2, { order: ORDER_DESCENDING });
+    });
+  }
+
+  return pastEvents;
 };
 
 export const sortUpcomingEvents = (events) => {
   let upcomingEvents = [];
 
   if (Array.isArray(events) && events.length) {
+    const getSortDate = (start, end) => {
+      const startDate = new Date(start);
+      const endDate = new Date(end);
+
+      if (startDate < new Date(Date.now()) && endDate) {
+        return endDate;
+      }
+
+      return startDate;
+    };
+
     upcomingEvents = [...events].sort((event1, event2) => {
       const sortDate1 = getSortDate(event1?.startDate, event1?.endDate);
       const sortDate2 = getSortDate(event2?.startDate, event2?.endDate);
-
-      if (sortDate1 > sortDate2) {
-        return 1;
-      }
-
-      if (sortDate1 < sortDate2) {
-        return -1;
-      }
-
-      return 0;
+      return sortDates(sortDate1, sortDate2, { order: ORDER_ASCENDING });
     });
   }
 
