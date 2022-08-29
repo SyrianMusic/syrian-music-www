b903830914e8e17d6d1287f79beeb9cd132c6ded
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-08-28 20:58:50 -0400

    user faker for performance utils

--- pages/performance/__tests__/utils.test.js
+++ pages/performance/__tests__/utils.test.js
@@ -1,23 +1,28 @@
-import { getNextEvent, sortUpcomingEvents } from '../utils';
-import { addDays, mockDateNow, nextWeek, tomorrow, yesterday } from '../../../__fixtures__/date';
+import faker from '../../../utils/faker';
 import { Event } from '../../../__fixtures__/Event';
+import { sortUpcomingEvents } from '../utils';
 
 describe('sortUpcomingEvents', () => {
-  mockDateNow();
-
-  const nextEvent = new Event({ startDate: tomorrow.toISOString() });
-
   it('filters out past events', () => {
-    const pastEvent = new Event({ startDate: yesterday.toISOString() });
-    const actual = sortUpcomingEvents([pastEvent, nextEvent]);
+    const futureDate = faker.date.future();
+    const pastDate = faker.date.past();
+    const futureEvent = new Event({ startDate: futureDate.toISOString() });
+    const pastEvent = new Event({ startDate: pastDate.toISOString() });
+
+    const actual = sortUpcomingEvents([pastEvent, futureEvent]);
+
     expect(actual.length).toBe(1);
-    expect(actual[0]).toBe(nextEvent);
+    expect(actual[0]).toBe(futureEvent);
   });
 
   it('sorts the upcoming events', () => {
-    const nextEvent = new Event({ startDate: tomorrow.toISOString() });
-    const followingEvent = new Event({ startDate: nextWeek.toISOString() });
+    const nextDate = faker.date.future();
+    const followingDate = faker.date.future(undefined, nextDate);
+    const nextEvent = new Event({ startDate: nextDate.toISOString() });
+    const followingEvent = new Event({ startDate: followingDate.toISOString() });
+
     const actual = sortUpcomingEvents([followingEvent, nextEvent]);
+
     expect(actual[0]).toBe(nextEvent);
     expect(actual[1]).toBe(followingEvent);
   });
