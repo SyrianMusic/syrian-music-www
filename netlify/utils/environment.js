ef8f7f354fb2d521eeb4c7f5bdcfa3058d90fd5f
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-08-29 22:28:17 -0400

    add JWT client secret to server

--- netlify/utils/environment.js
+++ netlify/utils/environment.js
@@ -3,5 +3,6 @@ const nodeEnv = process.env.NEXT_PUBLIC_NODE_ENV || process.env.NODE_ENV;
 const isProduction = nodeEnv === 'production';
 
 module.exports = {
+  jwtClientSecret: process.env.JWT_CLIENT_SECRET,
   stripePublishableKey: isProduction ? process.env.STRIPE_KEY_LIVE : process.env.STRIPE_KEY_TEST,
 };
