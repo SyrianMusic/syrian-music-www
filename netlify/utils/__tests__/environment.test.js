bf5840a91c5b8509747a788cb0215649b1057451
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-08-30 01:25:27 -0400

    test stripe secret key env var

--- netlify/utils/__tests__/environment.test.js
+++ netlify/utils/__tests__/environment.test.js
@@ -67,4 +67,35 @@ describe('environment', () => {
       },
     );
   });
+
+  describe('stripeSecretKey', () => {
+    const STRIPE_SECRET_KEY_LIVE = 'STRIPE_SECRET_KEY_LIVE';
+    const STRIPE_SECRET_KEY_TEST = 'STRIPE_SECRET_KEY_TEST';
+
+    const setupStripeEnvVars = () => {
+      process.env.STRIPE_SECRET_KEY_LIVE = STRIPE_SECRET_KEY_LIVE;
+      process.env.STRIPE_SECRET_KEY_TEST = STRIPE_SECRET_KEY_TEST;
+    };
+
+    it(`when the NODE_ENV is "${NODE_ENVS.PRODUCTION}", then it is set to STRIPE_SECRET_KEY_LIVE`, () => {
+      process.env.NODE_ENV = NODE_ENVS.PRODUCTION;
+      setupStripeEnvVars();
+      jest.isolateModules(() => {
+        environment = require('../environment').default;
+      });
+      expect(environment.stripeSecretKey).toBe(STRIPE_SECRET_KEY_LIVE);
+    });
+
+    it.each([NODE_ENVS.DEVELOPMENT, NODE_ENVS.PREVIEW, NODE_ENVS.TEST])(
+      'when the NODE_ENV is "%s", then it is set to STRIPE_SECRET_KEY_TEST',
+      (nodeEnv) => {
+        process.env.NODE_ENV = nodeEnv;
+        setupStripeEnvVars();
+        jest.isolateModules(() => {
+          environment = require('../environment').default;
+        });
+        expect(environment.stripeSecretKey).toBe(STRIPE_SECRET_KEY_TEST);
+      },
+    );
+  });
 });
