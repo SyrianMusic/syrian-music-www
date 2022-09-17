9faa3f5989504f8360385da438205d00a3abf740
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-09-17 11:42:50 -0400

    move match media mock

--- pages/performance/__tests__/PerformancePage.test.js
+++ pages/performance/__tests__/PerformancePage.test.js
@@ -1,4 +1,4 @@
-import './matchMedia.mock';
+import '../__helpers__/matchMedia.mock';
 import { render, screen } from '@testing-library/react';
 import faker from '../../../utils/faker';
 import { Event, EventCollection } from '../../../__fixtures__/Event';
