dcb7150c9b93fd860c811a5693692f5b70e60020
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-09-26 00:51:25 -0400

    create button styles (#164)
    * create button styles
    
    * use focus color in unstyled button
    
    * reduce border width

--- components/__stories__/Button.stories.js
+++ components/__stories__/Button.stories.js
@@ -1,6 +1,5 @@
-import React from 'react';
-
 import Button from '../Button';
+import './grid.css';
 
 export default {
   title: 'Button',
@@ -9,151 +8,62 @@ export default {
     controls: { expanded: true },
     layout: 'centered',
   },
-  argTypes: {
-    className: {
-      control: { type: null },
-    },
-    children: {
-      defaultValue: 'Children',
-      control: { type: 'text' },
-    },
-    color: {
-      control: { type: 'radio', options: Button.colors },
-    },
-    onClick: {
-      control: { type: null },
-    },
-    variant: {
-      control: { type: 'radio', options: Button.variants },
-    },
-  },
 };
+
+export const All = () => (
+  <div className="story-grid">
+    <div>
+      <Button>Default</Button>
+    </div>
+    <div className="pseudo-hover">
+      <Button>Hover</Button>
+    </div>
+    <div className="pseudo-focus">
+      <Button>Focus</Button>
+    </div>
+    <div className="pseudo-active">
+      <Button>Active</Button>
+    </div>
+    <div className="pseudo-hover pseudo-focus">
+      <Button>Hover Focus</Button>
+    </div>
+    <div className="pseudo-hover pseudo-active">
+      <Button>Hover Active</Button>
+    </div>
+    <div className="pseudo-focus pseudo-active">
+      <Button>Focus Active</Button>
+    </div>
+    <div className="pseudo-hover pseudo-focus pseudo-active">
+      <Button>Hover Focus Active</Button>
+    </div>
+    <div className="disabled">
+      <Button disabled>Disabled</Button>
+    </div>
+  </div>
+);
 
 const Template = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = { children: 'Label' };
 
-export const Primary = Template.bind({});
-Primary.args = {
-  color: 'primary',
-};
-Primary.argTypess = {
-  color: { control: { type: null } },
-};
+export const Hover = Template.bind({});
+Hover.args = { ...Default.args, pseudo: { hover: true } };
 
-export const PrimaryDisabled = Template.bind({});
-PrimaryDisabled.args = {
-  color: 'primary',
-  disabled: true,
-};
-PrimaryDisabled.argTypes = {
-  color: { control: { type: null } },
-  disabled: { control: { type: null } },
-};
+export const Active = Template.bind({});
+Active.args = { ...Default.args, pseudo: { active: true } };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  color: 'secondary',
-};
-Secondary.argTypess = {
-  color: { control: { type: null } },
-};
+export const Focus = Template.bind({});
+Focus.args = { ...Default.args, pseudo: { focus: true } };
 
-export const SecondaryDisabled = Template.bind({});
-SecondaryDisabled.args = {
-  color: 'secondary',
-  disabled: true,
-};
-SecondaryDisabled.argTypes = {
-  color: { control: { type: null } },
-  disabled: { control: { type: null } },
-};
+export const HoverFocus = Template.bind({});
+HoverFocus.args = { ...Default.args, pseudo: { hover: true, focus: true } };
 
-export const FilledPrimary = Template.bind({});
-FilledPrimary.args = {
-  color: 'primary',
-  variant: 'filled',
-};
-FilledPrimary.argTypes = {
-  color: { control: { type: null } },
-  variant: { control: { type: null } },
-};
+export const HoverActive = Template.bind({});
+HoverActive.args = { ...Default.args, pseudo: { hover: true, active: true } };
 
-export const FilledPrimaryDisabled = Template.bind({});
-FilledPrimaryDisabled.args = {
-  color: 'primary',
-  disabled: true,
-  variant: 'filled',
-};
-FilledPrimaryDisabled.argTypes = {
-  color: { control: { type: null } },
-  disabled: { control: { type: null } },
-  variant: { control: { type: null } },
-};
+export const HoverFocusActive = Template.bind({});
+HoverFocusActive.args = { ...Default.args, pseudo: { hover: true, focus: true, active: true } };
 
-export const FilledSecondary = Template.bind({});
-FilledSecondary.args = {
-  color: 'secondary',
-  variant: 'filled',
-};
-FilledSecondary.argTypes = {
-  color: { control: { type: null } },
-  variant: { control: { type: null } },
-};
-
-export const FilledSecondaryDisabled = Template.bind({});
-FilledSecondaryDisabled.args = {
-  color: 'secondary',
-  disabled: true,
-  variant: 'filled',
-};
-FilledSecondaryDisabled.argTypes = {
-  color: { control: { type: null } },
-  disabled: { control: { type: null } },
-  variant: { control: { type: null } },
-};
-
-export const OutlinedPrimary = Template.bind({});
-OutlinedPrimary.args = {
-  color: 'primary',
-  variant: 'outlined',
-};
-OutlinedPrimary.argTypes = {
-  color: { control: { type: null } },
-  variant: { control: { type: null } },
-};
-
-export const OutlinedPrimaryDisabled = Template.bind({});
-OutlinedPrimaryDisabled.args = {
-  color: 'primary',
-  disabled: true,
-  variant: 'outlined',
-};
-OutlinedPrimaryDisabled.argTypes = {
-  color: { control: { type: null } },
-  disabled: { control: { type: null } },
-  variant: { control: { type: null } },
-};
-
-export const OutlinedSecondary = Template.bind({});
-OutlinedSecondary.args = {
-  color: 'secondary',
-  variant: 'outlined',
-};
-OutlinedSecondary.argTypes = {
-  color: { control: { type: null } },
-  variant: { control: { type: null } },
-};
-
-export const OutlinedSecondaryDisabled = Template.bind({});
-OutlinedSecondaryDisabled.args = {
-  color: 'secondary',
-  disabled: true,
-  variant: 'outlined',
-};
-OutlinedSecondaryDisabled.argTypes = {
-  color: { control: { type: null } },
-  disabled: { control: { type: null } },
-  variant: { control: { type: null } },
-};
+export const Disabled = Template.bind({});
+Disabled.args = { ...Default.args, disabled: true };
