40e13b7d3d2eb77b67795fefb473699f3a63df01
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-09-30 00:36:21 -0400

    Revert "wait for error text in donate page stories"
    This reverts commit 2a57e9f1f9bde964d4cb0d2202c0bdc8cb1f5841.

--- pages/donate/__stories__/DonatePage.stories.js
+++ pages/donate/__stories__/DonatePage.stories.js
@@ -1,5 +1,4 @@
 import { action } from '@storybook/addon-actions';
-import { expect } from '@storybook/jest';
 import { userEvent, waitFor, within } from '@storybook/testing-library';
 import App from '../../_app.page';
 import { pageParameters } from '../../__stories__/config';
@@ -42,7 +41,6 @@ InputErrors.play = async ({ canvasElement }) => {
   await userEvent.clear(canvas.getByLabelText('Your Contribution'));
   await userEvent.tab({ shift: true });
   await userEvent.tab({ shift: true });
-  await waitFor(() => expect(canvas.queryByText('Please fill out this field.')).toBe(true));
 };
 
 // TODO: This is causing Chromatic to fail
