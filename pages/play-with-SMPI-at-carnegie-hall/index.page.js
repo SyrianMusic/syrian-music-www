fd1569456d20ad892d457fe4a3b01417a287f851
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-10-02 12:06:32 -0400

    add carnegie hall landing page

---
+++ pages/play-with-SMPI-at-carnegie-hall/index.page.js
@@ -0,0 +1,14 @@
+import SiteLayout from '../../components/SiteLayout';
+import Typography from '../../components/Typography';
+
+const CarnegieLandingPage = () => {
+  return (
+    <SiteLayout>
+      <Typography variant="h1" textAlign="center">
+        Play with SMPI at Carnegie Hall
+      </Typography>
+    </SiteLayout>
+  );
+};
+
+export default CarnegieLandingPage;
