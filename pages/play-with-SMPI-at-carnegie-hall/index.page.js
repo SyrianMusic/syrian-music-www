a1981b1cf97aa31f636e2f8a7da9f2e0d87109c6
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-10-02 12:57:27 -0400

    update body copy size

--- pages/play-with-SMPI-at-carnegie-hall/index.page.js
+++ pages/play-with-SMPI-at-carnegie-hall/index.page.js
@@ -15,6 +15,11 @@ const Section = styled.section([
   },
 ]);
 
+const Body = styled(Typography)({
+  fontSize: theme.pxToRem(15),
+  lineHeight: theme.pxToRem(20),
+});
+
 const Footnote = styled(Typography)({
   fontSize: theme.pxToRem(10),
   lineHeight: theme.pxToRem(12),
@@ -44,7 +49,7 @@ const CarnegieLandingPage = () => {
       <Section>
         <SectionHeader>Overview</SectionHeader>
 
-        <Typography>
+        <Body>
           Education is an important part of SMPI&rsquo;s mission. Because of this, we will be
           offering a unique opportunity as part of our Carnegie Hall performance on May 19, 2023.
           For one piece on our program, we will open the stage to advanced amateurs and enthusiasts
@@ -52,18 +57,18 @@ const CarnegieLandingPage = () => {
           <TBD>DATE-TBD</TBD> and if accepted, will participate in a number of workshops and special
           rehearsals in addition to the dress rehearsal<sup>*</sup> at Carnegie Hall and the
           performance.
-        </Typography>
+        </Body>
 
-        <Typography>
+        <Body>
           The application will require a fee of $50 and those who pass the audition will be asked to
           donate $500 to cover the costs of participation.
-        </Typography>
+        </Body>
 
-        <Typography>
+        <Body>
           Participants will be able to purchase tickets within a block of seats dedicated to winners
           and their guests. Participants and 2 guests will be invited to attend the post-concert
           reception at Carnegie Hall.
-        </Typography>
+        </Body>
 
         <Footnote>
           <sup>*</sup> Attendance at all workshops and rehearsals will be required for final
@@ -82,25 +87,25 @@ const CarnegieLandingPage = () => {
       <Section>
         <SectionHeader>How To Apply</SectionHeader>
 
-        <Typography>
+        <Body>
           <TBD>Download your application</TBD> and submit it to{' '}
           <a href="mailto:info@syrianmusic.org?subject=Carnegie Hall Audition">
             info@syrianmusic.org
           </a>{' '}
           no later than 11:59PM EST on January 2, 2023.
-        </Typography>
+        </Body>
 
-        <Typography>
+        <Body>
           <Link href="/donate">
             <a>Donate</a>
           </Link>{' '}
           $50 by <TBD>January XX, 2023</TBD>, for your application fee.
-        </Typography>
+        </Body>
 
-        <Typography>
+        <Body>
           Only <TBD>XX</TBD> number of applications will be accepted. If your application is not
           accepted, your application fee will be refunded.
-        </Typography>
+        </Body>
       </Section>
 
       <Section>
