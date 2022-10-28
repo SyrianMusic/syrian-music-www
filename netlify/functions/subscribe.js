eb5f606f6f6b7095614df2453a5c5e06b02f2cfa
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-10-28 00:06:22 -0400

    create subscribe function

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -1,17 +1,31 @@
 import { withAuth } from '../utils/auth';
 import EmailService from '../utils/email';
 import { STATUS_CODES } from '../utils/http';
+import logger from '../utils/logger';
 
 const subscribe = async (event) => {
   const { httpMethod } = event;
   if (httpMethod !== 'POST') return { statusCode: STATUS_CODES.METHOD_NOT_ALLOWED };
 
-  const res = await EmailService().ping();
+  try {
+    const { email } = JSON.parse(event.body);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(res),
-  };
+    const { error } = await EmailService().subscribe({ email });
+
+    if (error) throw error;
+
+    return { statusCode: 201 };
+  } catch (error) {
+    logger.error(error);
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify({
+        error: {
+          message: error.message,
+        },
+      }),
+    };
+  }
 };
 
 export const handler = withAuth(subscribe);
--- netlify/utils/email.js
+++ netlify/utils/email.js
@@ -1,5 +1,8 @@
-import mailchimp from '@mailchimp/mailchimp_marketing';
+const mailchimp = require('@mailchimp/mailchimp_marketing');
 import { mailchimpKey } from './environment';
+import logger from './logger';
+
+const LIST_ID = '66a839666b';
 
 mailchimp.setConfig({
   apiKey: mailchimpKey,
@@ -10,6 +13,38 @@ const EmailService = () => {
   return {
     async ping() {
       return await mailchimp.ping.get();
+    },
+
+    async subscribe({ email } = {}) {
+      try {
+        const res = await mailchimp.lists.setListMember(LIST_ID, email, {
+          email_address: email,
+          status_if_new: 'subscribed',
+          status: 'subscribed',
+        });
+        logger.info(res);
+        return res;
+      } catch (e) {
+        logger.error(JSON.stringify(e));
+
+        const mailchimpError = JSON.parse(e.response.text);
+
+        const error = {
+          status: mailchimpError.status,
+          code: mailchimpError.title,
+          message: mailchimpError.detail,
+        };
+
+        logger.error('Mailchimp returned an error:', error);
+
+        return {
+          error: {
+            status: error.status || 500,
+            message:
+              'Unfortunately, we are unable to sign you up to our mailing list. Refresh the page and try again with a different email address.',
+          },
+        };
+      }
     },
   };
 };
