be6b612c5654758d832afffa6e9f1688bc928b36
Erik Jönsson <13211361+erikwithuhk@users.noreply.github.com>
2022-10-28 00:26:20 -0400

    add subscribe to front end API

--- api/index.js
+++ api/index.js
@@ -20,5 +20,21 @@ export const createPaymentIntent = async ({ amount, description, idempotencyKey 
   return { clientSecret };
 };
 
+export const subscribe = async ({ component, email, url }) => {
+  const res = await fetch('/.netlify/functions/subscribe', {
+    method: 'POST',
+    headers: { authorization, 'Content-Type': 'application/json' },
+    body: JSON.stringify({ component, email, url }),
+  });
+
+  if (!res.ok) throw new Error(res.statusText);
+
+  const { error } = await res.json();
+
+  if (error) throw new Error(error.message);
+
+  return { ok: true };
+};
+
 // TODO: Move to Contentful Service
 export { BaseAPI } from './BaseAPI';
